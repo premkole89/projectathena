555168f6bba102eea6beaf746db0dfe41db48eac
premkole89 <115562044+premkole89@users.noreply.github.com>
2022-10-11 21:13:51 +0530

    Add files via upload

--- addition.py
+++ addition.py
@@ -1,6 +1,5 @@
 print('Enter your first number')
 num1= input()
-print('Enter the dragon')
 print('Enter your second number')
 num2= input()
 print("Total number is ",int(num1)+int(num2))
---
+++ append.py
@@ -0,0 +1,15 @@
+nums = []
+nums.append(8)
+nums.append(8989)
+nums.append("Happy birthday Prem")
+coding = []
+coding.append("self build")
+num =[]
+num.append("speed" )
+num.append("okits")
+
+
+print(nums)
+print(coding)
+print(num)
+
